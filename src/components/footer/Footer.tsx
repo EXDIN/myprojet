6e3c5d642e69cb324cf825ebf5b80406307e7e40
EXDIN <exdin1@gmail.com>
2024-09-17 22:38:17 +0300

    create footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import styles from "./footer.module.css"
+import linkdn from "../../image/linkedin.png"
+
+const myLinkd = "https://www.linkedin.com/in/ivan-lila/"
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <a href={myLinkd}>
+        <img src={linkdn} className={styles.linked}/>
+      </a>
+      <text>© Blizzard Entertainment, Inc., 2024</text>
+    </div>
+  )
+}
