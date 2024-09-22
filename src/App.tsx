61abe7941ae906fc06f4a4671a4ca6de436b2dca
EXDIN <exdin1@gmail.com>
2024-09-22 18:00:11 +0300

    Add page 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './mainpage/MainPage';
 import Registration from './registrationpage/Registration';
 import Authorization from './authorizationpage/Authorization';
+import Page404 from './page404/Page404';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <MainPage/>} />
-        <Route path="/registration" element={ <Registration/>} />
-        <Route path="/authorization" element={ <Authorization/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={ <MainPage/>} />
+          <Route path="/registration" element={ <Registration/>} />
+          <Route path="/authorization" element={ <Authorization/>} />
+          <Route path="/*" element={ <Page404/>} />
+        </Routes>
     </BrowserRouter>
   )
 }
