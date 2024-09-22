9883e208164de58f93cc2aa99bec8dc9394edcaf
EXDIN <exdin1@gmail.com>
2024-09-22 18:00:57 +0300

    add types file

---
+++ src/typesAndInterfaces.ts
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+export type UserDataToLog = {
+  email: string,
+  password: string
+}
+
+export type UserData = {
+  name: string,
+  age: string,
+  email: string,
+  password: string
+}
+
+export interface IMyComponentProps {
+  children: ReactNode;
+}
+
+export type typeUserData = {
+  name: string,
+  isAuth: boolean,
+}
+
+export type TypeUserContext = {
+  user: typeUserData
+  updateUser: (newUser: {isAuth: boolean, name: string}) => void,
+}
