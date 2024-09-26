2ab294379aeefcccd5a58014e8e4a55c3a6a6dcd
EXDIN <exdin1@gmail.com>
2024-09-26 23:30:40 +0300

    update db

--- src/components/urls.tsx
+++ src/components/urls.tsx
@@ -1,4 +1,6 @@
 const myLocalJsonServer = "http://localhost:3005/users"
+export const myLocalArticles = "http://localhost:3005/articles"
+
 export const myLinkd = "https://www.linkedin.com/in/ivan-lila/"
 
 export default myLocalJsonServer
